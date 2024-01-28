02b545de4cd74cec79bb901b8b927bf63fb5c3c5
martin265 <martinsilungwe12@gmail.com>
2024-01-28 10:57:26 +0200

    in progress

--- administrator_administer_GAD_7.php
+++ administrator_administer_GAD_7.php
@@ -132,25 +132,248 @@
                                             </label>
                                             <!-- =============== the other radio button will be here ======== -->
                                             <div class="form-check form-check-inline ms-3">
-                                                <input class="form-check-input" type="radio" name="how_often" id="inlineRadio1" value="Daily">
+                                                <input class="form-check-input" type="radio" name="recreation_how_often" id="inlineRadio1" value="Daily">
                                                 <label class="form-check-label" for="inlineRadio1">Daily</label>
                                             </div>
                                             <!-- ============== the radio buttons will be here -->
                                             <div class="form-check form-check-inline">
-                                                <input class="form-check-input" type="radio" name="how_often" id="inlineRadio2" value="Weekly">
+                                                <input class="form-check-input" type="radio" name="recreation_how_often" id="inlineRadio2" value="Weekly">
                                                 <label class="form-check-label" for="inlineRadio2">Weekly</label>
                                             </div>
                                             <!-- ============== the radio buttons will be here -->
                                             <div class="form-check form-check-inline">
-                                                <input class="form-check-input" type="radio" name="how_often" id="inlineRadio2" value="Monthly">
+                                                <input class="form-check-input" type="radio" name="recreation_how_often" id="inlineRadio2" value="Monthly">
                                                 <label class="form-check-label" for="inlineRadio2">Monthly</label>
                                             </div>
                                             <!-- ============== the radio buttons will be here -->
                                             <div class="form-check form-check-inline">
-                                                <input class="form-check-input" type="radio" name="how_often" id="inlineRadio2" value="Infrequently">
+                                                <input class="form-check-input" type="radio" name="recreation_how_often" id="inlineRadio2" value="Infrequently">
                                                 <label class="form-check-label" for="inlineRadio2">Infrequently</label>
                                             </div>
                                         </div>
+                                    </div>
+
+                                    <!-- ================ what type of drugs do you take =========== -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3 me-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold">
+                                                    Which drugs do you use ?                                               </span>
+                                            </label>
+                                            <div class="input-group">
+                                                <span class="input-group-text"></span>
+                                                <input type="text" name="types of drugs" class="form-control form-control-lg" placeholder="drugs you use...">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- ================= CAGE questions section here =========== -->
+                                    <div class="cage-questions-section">
+                                        <h1>cage questions</h1>
+                                    </div>
+                                    <!-- ============ CAGE SECTION HERE ============== -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold me-5">
+                                                    C - Ever try to Cut Back on your drinking or grug use ?
+                                                </span>
+                                            </label>
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="checkbox" name="cage_questions" id="inlineRadio2" value="Cut Back">
+                                                <label class="form-check-label" for="inlineRadio2">Cut Back</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- ===================== // ======================= // -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold me-5">
+                                                    A - Ever been Annoyed by anyone about your drinking or drug use ?
+                                                </span>
+                                            </label>
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="checkbox" name="cage_questions" id="inlineRadio2" value="Annoyed">
+                                                <label class="form-check-label" for="inlineRadio2">Annoyed</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- ===================== // ======================= // -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold me-5">
+                                                    G - Ever felt Guilty or ashamed about your drinking or drug use ?
+                                                </span>
+                                            </label>
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="checkbox" name="cage_questions" id="inlineRadio2" value="Guilty">
+                                                <label class="form-check-label" for="inlineRadio2">Guilty</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- ===================== // ======================= // -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold me-5">
+                                                    E - Ever had an 'Eye-opener' or used alcohol or drugs in the morning ?
+                                                </span>
+                                            </label>
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="checkbox" name="cage_questions" id="inlineRadio2" value="Eye-opener">
+                                                <label class="form-check-label" for="inlineRadio2">Eye-opener</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- ===================== the use of drugs here ====================-->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3 me-3">
+                                            <label for="Age" class="form-label-lg">
+                                                <span class="fw-bold">Have you sought help before for any alcohol/drug issue ?</span>
+                                            </label>
+                                            <!-- =============== the other radio button will be here ======== -->
+                                            <div class="form-check form-check-inline ms-3">
+                                                <input class="form-check-input" type="radio" name="help" id="inlineRadio1" value="Yes">
+                                                <label class="form-check-label" for="inlineRadio1">Yes</label>
+                                            </div>
+                                            <!-- ============== the radio buttons will be here -->
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio" name="help" id="inlineRadio2" value="No">
+                                                <label class="form-check-label" for="inlineRadio2">No</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- =============== the section for the emotions here ============ -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3 me-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold">
+                                                    If so, When ?                             
+                                                </span>
+                                            </label>
+                                            <div class="input-group">
+                                                <span class="input-group-text"></span>
+                                                <input type="text" name="when" class="form-control form-control-lg" placeholder="when...">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- =============== the section for the emotions here ============ -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3 me-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold">
+                                                    If so Where ?                             
+                                                </span>
+                                            </label>
+                                            <div class="input-group">
+                                                <span class="input-group-text"></span>
+                                                <input type="text" name="where" class="form-control form-control-lg" placeholder="where...">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- ==================== gamble section here ============== -->
+
+                                    <!-- ===================== the use of drugs here ====================-->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3 me-3">
+                                            <label for="Age" class="form-label-lg">
+                                                <span class="fw-bold">Do you gamble ?</span>
+                                            </label>
+                                            <!-- =============== the other radio button will be here ======== -->
+                                            <div class="form-check form-check-inline ms-3">
+                                                <input class="form-check-input" type="radio" name="gamble" id="inlineRadio1" value="Yes">
+                                                <label class="form-check-label" for="inlineRadio1">Yes</label>
+                                            </div>
+                                            <!-- ============== the radio buttons will be here -->
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio" name="gamble" id="inlineRadio2" value="No">
+                                                <label class="form-check-label" for="inlineRadio2">No</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- =============== the section for the emotions here ============ -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3 me-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold">
+                                                    If Yes, What type ?                             
+                                                </span>
+                                            </label>
+                                            <div class="input-group">
+                                                <span class="input-group-text"></span>
+                                                <input type="text" name="what_type" class="form-control form-control-lg" placeholder="what type...">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- =============== the section for the emotions here ============ -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3 me-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold">
+                                                    If Yes, How long ?                             
+                                                </span>
+                                            </label>
+                                            <div class="input-group">
+                                                <span class="input-group-text"></span>
+                                                <input type="text" name="how long" class="form-control form-control-lg" placeholder="how long...">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- =============== the section for the emotions here ============ -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3 me-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold">
+                                                    Is there anything you are addicted to that worries you ?                             
+                                                </span>
+                                            </label>
+                                            <div class="input-group">
+                                                <span class="input-group-text"></span>
+                                                <input type="text" name="addicted" class="form-control form-control-lg" placeholder="addictions...">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- =============== the section for the emotions here ============ -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3 me-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold">
+                                                    If so Where ?                             
+                                                </span>
+                                            </label>
+                                            <div class="input-group">
+                                                <span class="input-group-text"></span>
+                                                <input type="text" name="concentration" class="form-control form-control-lg" placeholder="concentration...">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- =============== the section for the emotions here ============ -->
+                                    <div class="row mb-3">
+                                        <div class="col ms-3 me-3">
+                                            <label for="ForShorttempred">
+                                                <span class="fw-bold">
+                                                    How would you describe your memory?                             
+                                                </span>
+                                            </label>
+                                            <div class="input-group">
+                                                <span class="input-group-text"></span>
+                                                <input type="text" name="memory" class="form-control form-control-lg" placeholder="memory...">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="save-details-btn mt-3 ms-3">
+                                        <input type="submit" class="btn btn-lg btn-success" name="save_session" value="save session">
                                     </div>
                                 </form>
                             </div>
